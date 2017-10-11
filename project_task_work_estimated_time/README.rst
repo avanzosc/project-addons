62cf3a259ecdac63fe0d414af24218557f09a05d
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-10-11 09:43:44 +0200

    [ADD] project_task_work_estimated_time: New field "Estimated time" in "Project Task Work".

---
+++ project_task_work_estimated_time/README.rst
@@ -0,0 +1,21 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================================
+Project task work estimated time
+================================
+
+* New field "Estimated time" in "Project Task Work".
+* When copying a task, copy its imputations, putting the new field "Estimated
+  time" to zero.
+
+
+Credits
+=======
+
+
+Contributors
+------------
+* Ana Juaristi <ajuaristio@gmail.com>
+* Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
---
+++ project_task_work_estimated_time/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project task work estimated time",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "depends": [
+        "project_timesheet",
+    ],
+    "data": [
+        "views/project_task_view.xml",
+        "views/project_task_work_view.xml",
+    ],
+    "installable": True,
+}
