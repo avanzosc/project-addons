493b147b4cde5b688de8c1fb4f4757a73b1e8f89
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-07-25 11:27:04 +0200

    [IMP] project_partner_rol: add planned hours info

--- project_partner_rol/models/project.py
+++ project_partner_rol/models/project.py
@@ -1,6 +1,8 @@
 # Copyright 2018 Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.addons import decimal_precision as dp
 
 
 class ProjectProject(models.Model):
@@ -11,6 +13,23 @@ class ProjectProject(models.Model):
     participant_ids = fields.One2many(
         comodel_name='project.participant',
         inverse_name='project_id', string='Participants')
+    planned_hours = fields.Float(
+        string='Initially Planned Hours',
+        help='Estimated time to do the task, usually set by the project '
+             'manager.')
+    task_planned_hours = fields.Float(
+        string='Initially Planned Hours',
+        help='Estimated time to do the task, usually set by the project '
+             'manager when the task is in draft state.',
+        compute='_compute_task_planned_hours')
+
+    @api.multi
+    def _compute_task_planned_hours(self):
+        tasks = self.env['project.task'].search([])
+        for project in self:
+            project.task_planned_hours = sum(
+                tasks.filtered(lambda t: t.project_id.id == project.id).mapped(
+                    'planned_hours'))
 
 
 class ProjectParticipant(models.Model):
@@ -24,7 +43,41 @@ class ProjectParticipant(models.Model):
         comodel_name='res.partner', string='Participant', required=True)
     rol_id = fields.Many2one(
         comodel_name='project.participant.rol', string='Role')
+    planned_hours_percentage = fields.Float(
+        string='Percentage (%)', digits=dp.get_precision('Discount'))
+    project_planned_hours = fields.Float(
+        string='Initially Planned Hours',
+        compute='_compute_project_planned_hours')
+    task_planned_hours = fields.Float(
+        string='Initially Planned Hours',
+        compute='_compute_task_planned_hours')
 
+    @api.multi
+    @api.constrains('planned_hours_percentage')
+    def _check_percent(self):
+        for record in self:
+            if (record.planned_hours_percentage < 0.0 or
+                    record.planned_hours_percentage > 100.0):
+                raise ValidationError(
+                    _('Percentages for must be between 0 and 100.'))
+
+    @api.multi
+    @api.depends('project_id', 'project_id.planned_hours',
+                 'planned_hours_percentage')
+    def _compute_project_planned_hours(self):
+        for participant in self.filtered('planned_hours_percentage'):
+            participant.project_planned_hours = (
+                participant.project_id.planned_hours *
+                (participant.planned_hours_percentage / 100.0))
+
+    @api.multi
+    @api.depends('project_id', 'project_id.task_planned_hours',
+                 'planned_hours_percentage')
+    def _compute_task_planned_hours(self):
+        for participant in self.filtered('planned_hours_percentage'):
+            participant.task_planned_hours = (
+                participant.project_id.task_planned_hours *
+                (participant.planned_hours_percentage / 100.0))
 
 class ProjectParticipantRol(models.Model):
     _name = 'project.participant.rol'
