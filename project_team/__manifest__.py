7aa93e688aac0d2e6d24ee7fd73bd83c746748af
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-08-20 16:13:36 +0200

    [ADD] New module <project_team>

---
+++ project_team/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Team",
+    "version": "11.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_member_view.xml",
+        "views/project_project_view.xml",
+    ],
+    "installable": True,
+}
