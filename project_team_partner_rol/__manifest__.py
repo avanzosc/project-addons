63d9f2fd51762b3977ae31d0f420b5e7a43bed40
oihane <oihanecruce@gmail.com>
2018-08-28 13:49:20 +0200

    [ADD] New module project_team_partner_rol (#36)
    * [ADD] New module project_team_partner_rol
    
    * [IMP] project_team_partner_rol: add translations

---
+++ project_team_partner_rol/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Team Partner Rol",
+    "version": "11.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "project_team",
+        "project_partner_rol",
+    ],
+    "data": [
+        "views/project_project_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
