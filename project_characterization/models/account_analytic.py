4d934ca86dcbedf5d3dca0d55673e0f0b62ab4f9
oihane <oihanecruce@gmail.com>
2018-10-01 14:45:45 +0200

    [IMP] project_characterization: some view adjust (#37)
    * [IMP] project_characterization: some view adjust
    
    * [IMP] project_characterization: change views, create code by area and area type

--- project_characterization/models/account_analytic.py
+++ project_characterization/models/account_analytic.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Maite Esnal - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountAnalyticAccount(models.Model):
@@ -23,6 +23,21 @@ class AccountAnalyticAccount(models.Model):
         comodel_name='res.target', string='Target')
     nonoperative = fields.Boolean(
         string='Non Operative', related='res_area_id.nonoperative', store=True)
+
+    @api.model
+    def create(self, values):
+        area_id = values.get('res_area_id')
+        type_id = values.get('res_area_type_id')
+        if area_id and type_id:
+            count = self.search_count([('res_area_id', '=', area_id),
+                                       ('res_area_type_id', '=', type_id)])
+            values.update({
+                'code': '{}.{}.{}'.format(
+                    self.env['res.area'].browse(area_id).code or '',
+                    self.env['res.area.type'].browse(type_id).code or '',
+                    count+1)
+            })
+        return super(AccountAnalyticAccount, self).create(values)
 
 
 class AccountAnalyticLine(models.Model):
