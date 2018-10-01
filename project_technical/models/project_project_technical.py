212429d28cc5fa3449dfc259dc72dd4653cd9209
Gontzal Gomez <gontzalgomez@avanzosc.es>
2018-10-01 17:32:18 +0200

    [ADD] project_technical: new module added

---
+++ project_technical/models/project_project_technical.py
@@ -0,0 +1,14 @@
+# Copyright 2018 Gontzal Gomez - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class ProjectProjectTechnical(models.Model):
+    _inherit = 'project.project'
+
+    customer_goal = fields.Char(string="Customer's Goal")
+    internal_goal = fields.Char(string='Internal Goal')
+    work_scope = fields.Html(string='Scope of Work')
+    definition = fields.Html(string='Technical Definition')
+    task_description = fields.Html(string='Description of Tasks')
