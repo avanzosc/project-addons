850031fafd82226b84d5ab89d49a6f23c7ece318
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-10-03 16:09:52 +0200

    [ADD] New module project_budget_characterization

---
+++ project_budget_characterization/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Bugdet Characterization",
+    "version": "11.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "project_budget",
+        "project_characterization",
+    ],
+    "data": [
+        "views/crossovered_budget_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
