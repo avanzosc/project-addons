b246a7ada51457c093ab08bc55cc278caba92d03
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-10-03 16:35:48 +0200

    [IMP] project_close_info: add readme

--- project_close_info/__manifest__.py
+++ project_close_info/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Project Close",
+    "name": "Project Closure",
     "version": "11.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
