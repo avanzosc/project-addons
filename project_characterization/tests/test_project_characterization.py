e5244d5e71e05674e745e0ad33eb0021b6e32a6a
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-10-23 00:29:52 +0200

    [FIX] project_characterization: fix errored travis

--- project_characterization/tests/test_project_characterization.py
+++ project_characterization/tests/test_project_characterization.py
@@ -2,7 +2,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo.tests import common
-from odoo.exceptions import ValidationError
 
 
 class TestProjectCharacterization(common.TransactionCase):
