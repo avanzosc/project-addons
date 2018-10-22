573ba3983170520bb1b026dbf029934c93960ad4
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-10-22 16:42:38 +0200

    [ADD] new module project_funding

---
+++ project_funding/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Funding",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "partner_funding_source",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Project",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/funding_source_project_view.xml",
+        "views/project_project_view.xml",
+        "views/project_funding_view.xml",
+    ],
+    "installable": True,
+}
