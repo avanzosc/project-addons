c284ae991bfdec9bc47dda806cb05a726f3b292a
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-10-25 17:01:11 +0200

    [IMP] project_idea: remove viability (moved to another module)

--- project_idea/models/__init__.py
+++ project_idea/models/__init__.py
@@ -4,7 +4,4 @@
 from . import project_project
 from . import project_idea_resource_human
 from . import project_idea_resource_material
-from . import project_idea_line_viability
-from . import project_idea_cat_viability
-from . import project_idea_viability_category
 from . import hr_department
--- project_idea/models/project_project.py
+++ project_idea/models/project_project.py
@@ -7,12 +7,6 @@ from odoo import fields, models
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    author_id = fields.Many2one(
-        string='Author', comodel_name='res.users')
-    idea_date = fields.Date(string='Date')
-    internal_idea_goal = fields.Char(string='Internal Goal')
-    customer_idea_goal = fields.Char(string="Customer's Goal")
-    technical_approach = fields.Text(string='Technical Approach')
     idea_type = fields.Selection(
         selection=[('training', 'Training'),
                    ('transference', 'Transference'),
@@ -21,10 +15,6 @@ class ProjectProject(models.Model):
     idea_origin = fields.Selection(
         selection=[('internal', 'Internal'),
                    ('customer', 'Customer')], string='Origin')
-    financing = fields.Selection(
-        selection=[('industrial', 'Industrial'),
-                   ('institutional', 'Institutional'),
-                   ('internal', 'Internal')], string='Financing Source')
     potential_customers = fields.Char(string='Potential Customers')
     spec_line_ids = fields.Many2many(
         string='Specialization Lines', comodel_name='hr.department',
@@ -37,11 +27,4 @@ class ProjectProject(models.Model):
         string='Material Resources',
         comodel_name='project.idea.resource.material',
         inverse_name='project_id')
-    lines_viability_ids = fields.One2many(
-        string='Feasibility Lines',
-        comodel_name='project.idea.line.viability',
-        inverse_name='project_id')
-    cat_viability_ids = fields.One2many(
-        string='Viability by Category',
-        comodel_name='project.idea.cat.viability',
-        inverse_name='project_id')
+    year_duration = fields.Integer(string='Estimated term (years)')
