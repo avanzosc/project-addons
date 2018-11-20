14c4b076ad670c7659add4d1ed2738a899e683ec
oihane <oihanecruce@gmail.com>
2018-11-20 08:55:25 +0100

    [IMP] project_goal: missing ondelete, some view optimization, etc (#87)

--- project_goal/models/project_goal.py
+++ project_goal/models/project_goal.py
@@ -7,12 +7,15 @@ from odoo import fields, models
 class ProjectObjetive(models.Model):
     _name = 'project.goal'
     _description = 'Goals of Projects'
+    _order = 'project_id,sequence'
 
-    project_id = fields.Many2one(comodel_name='project.project')
+    project_id = fields.Many2one(
+        name='Project', comodel_name='project.project', required=True,
+        ondelete='cascade')
     sequence = fields.Integer(string='Sequence')
     name = fields.Char(string='Name')
     type_id = fields.Many2one(
-        string='Type', comodel_name='project.goal.type')
+        string='Type', comodel_name='project.goal.type', required=True)
     acceptance_range = fields.Text(string='Acceptance range')
     compliance = fields.Char(string='Compliance Degree')
     remark = fields.Char(string='Remarks')
