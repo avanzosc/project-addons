64106ddea4dbe3ab1731437354d6a1956508b8ab
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-11-28 15:10:55 +0100

    [IMP] project_partner_rol: some tidy up

--- project_partner_rol/__manifest__.py
+++ project_partner_rol/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Partner Rol",
-    "version": "11.0.2.1.0",
+    "version": "11.0.2.1.1",
     "license": "AGPL-3",
     "author":  "AvanzOSC",
     "website": "http://www.avanzosc.es",
@@ -13,8 +13,8 @@
     "data": [
         "security/ir.model.access.csv",
         "security/project_security.xml",
-        "views/project_participant_view.xml",
         "views/project_project_view.xml",
+        "views/project_participant_view.xml",
         "views/project_participant_rol_view.xml",
         "views/res_config_settings_views.xml",
     ],
