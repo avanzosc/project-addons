b7e2b83693e26dcb51ecdbb82ef73228b2475814
Gontzal Gomez <gontzalgomez@avanzosc.es>
2018-12-04 16:24:07 +0100

    [ADD] project_task_cost: New module created (#93)
    * [ADD]project_task_time: New module created
    
    * [IMP] project_task_time: calculate all data with fixed cost to data when assigned user
    
    * [IMP] project_task_time: renamed files
    
    * [IMP] project_task_time: added tests
    
    * [IMP] project_task_cost: renamed module
    
    * [IMP] project_task_cost: added translation, fix test

---
+++ project_task_cost/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018 Gontzal Gomez - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Task Cost",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "hr",
+        "sale_timesheet",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Project",
+    "data": [
+        "views/project_task_view.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
---
+++ project_task_cost/_hooks.py
@@ -0,0 +1,11 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import SUPERUSER_ID
+from odoo.api import Environment
+
+
+def post_init_hook(cr, pool):
+    env = Environment(cr, SUPERUSER_ID, {})
+    for task in env['project.task'].search([]):
+        task._onchange_user()
