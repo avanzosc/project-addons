ab5b7ed3cfd3abefca424e1363c8e7fd76b7fcd7
Gontzal Gomez <gontzalgomez@avanzosc.es>
2018-12-04 16:24:07 +0100

    [ADD] project_task_cost: New module created (#93)
    * [ADD]project_task_time: New module created
    
    * [IMP] project_task_time: calculate all data with fixed cost to data when assigned user
    
    * [IMP] project_task_time: renamed files
    
    * [IMP] project_task_time: added tests
    
    * [IMP] project_task_cost: renamed module
    
    * [IMP] project_task_cost: added translation, fix test

---
+++ project_task_cost/models/project_task.py
@@ -0,0 +1,48 @@
+# Copyright 2018 Gontzal Gomez - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models
+
+str2date = fields.Datetime.from_string
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    employee_cost = fields.Float(string='Employee Cost')
+    planned_cost = fields.Float(
+        string='Estimated Cost', compute='_compute_planned_cost', store=True)
+    effective_cost = fields.Float(
+        string='Real Cost', compute='_compute_effective_cost', store=True)
+    planned_monthly_hours = fields.Float(
+        string='Planned Monthly Hours',
+        compute='_compute_planned_monthly_hours', store=True)
+
+    @api.onchange('user_id')
+    def _onchange_user(self):
+        res = super(ProjectTask, self)._onchange_user()
+        employee_model = self.env['hr.employee']
+        for task in self:
+            employee = employee_model.search(
+                [('user_id', '=', task.user_id.id)], limit=1)
+            task.employee_cost = employee.timesheet_cost
+        return res
+
+    @api.depends('employee_cost', 'planned_hours')
+    def _compute_planned_cost(self):
+        for task in self.filtered('user_id'):
+            task.planned_cost = task.planned_hours * task.employee_cost
+
+    @api.depends('employee_cost', 'effective_hours')
+    def _compute_effective_cost(self):
+        for task in self.filtered('user_id'):
+            task.effective_cost = task.effective_hours * task.employee_cost
+
+    @api.depends('date_start', 'date_end', 'planned_hours')
+    def _compute_planned_monthly_hours(self):
+        for task in self.filtered(lambda t: t.date_start and t.date_end):
+            months = (
+                relativedelta(str2date(task.date_end),
+                              str2date(task.date_start)).months + 1)
+            task.planned_monthly_hours = task.planned_hours / (months or 1.0)
