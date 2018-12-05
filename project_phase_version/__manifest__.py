c91b4c002ef984f310f3744ca0395f2649b8f806
oihane <oihanecruce@gmail.com>
2018-12-05 10:00:48 +0100

    [ADD] New module project_phase_version (#73)
    * [ADD] New module project_phase_version
    
    * [IMP] project_phase_version: create new version or historify according to phase
    
    * [IMP] project_phase_version: historify when phase change
    
    * [IMP] project_phase_version: tests added

---
+++ project_phase_version/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Phase Version",
+    "version": "11.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "project_version",
+        "project_phase",
+    ],
+    "data": [
+        "views/project_project_view.xml",
+        "views/res_config_settings_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ project_phase_version/tests/test_project_phase_version.py
@@ -0,0 +1,60 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo.tests import common
+
+
+class TestProjectVersion(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestProjectVersion, cls).setUpClass()
+        cls.project_model = cls.env['project.project']
+        cls.project = cls.project_model.create({
+            'name': 'Test Project',
+        })
+        phase_model = cls.env['project.phase']
+        cls.phase1 = cls.project_model._get_default_phase_id()
+        cls.phase2 = phase_model.search([('sequence', '!=', '1')], limit=1)
+        cls.setting = cls.env['res.config.settings'].create({})
+
+    def test_project_phase_nohistorify(self):
+        self.setting.phase_history = False
+        self.setting.set_values()
+        self.assertNotEquals(self.phase1, self.phase2)
+        self.assertEquals(self.phase1, self.project.phase_id)
+        history_versions = self.project_model.with_context(
+            active_test=False).search([
+                ('parent_id', '=', self.project.id),
+                '|', ('historical_user_id', '!=', False),
+                ('historical_date', '!=', False)
+            ])
+        self.assertEquals(len(history_versions), 0)
+        self.project.phase_id = self.phase2
+        history_versions = self.project_model.with_context(
+            active_test=False).search([
+                ('parent_id', '=', self.project.id),
+                '|', ('historical_user_id', '!=', False),
+                ('historical_date', '!=', False)
+            ])
+        self.assertEquals(len(history_versions), 0)
+
+    def test_project_phase_historify(self):
+        self.setting.phase_history = True
+        self.setting.set_values()
+        self.assertNotEquals(self.phase1, self.phase2)
+        self.assertEquals(self.phase1, self.project.phase_id)
+        history_versions = self.project_model.with_context(
+            active_test=False).search([
+                ('parent_id', '=', self.project.id),
+                '|', ('historical_user_id', '!=', False),
+                ('historical_date', '!=', False)
+            ])
+        self.assertEquals(len(history_versions), 0)
+        self.project.phase_id = self.phase2
+        history_versions = self.project_model.with_context(
+            active_test=False).search([
+                ('parent_id', '=', self.project.id),
+                '|', ('historical_user_id', '!=', False),
+                ('historical_date', '!=', False)
+            ])
+        self.assertEquals(len(history_versions), 1)
