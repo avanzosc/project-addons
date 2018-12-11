b2c09a7420fa0f4b51192d105d951c38f62b24d5
oihane <oihanecruce@gmail.com>
2018-12-11 15:11:07 +0100

    [IMP] project_partner_rol: team participans don't have estimated time (#103)
    * [IMP] project_partner_rol: team participans don't have estimated time
    
    * [IMP] project_partner_rol: remove unnecessary test

--- project_partner_rol/__manifest__.py
+++ project_partner_rol/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Partner Rol",
-    "version": "11.0.2.1.1",
+    "version": "11.0.2.2.0",
     "license": "AGPL-3",
     "author":  "AvanzOSC",
     "website": "http://www.avanzosc.es",
@@ -12,11 +12,9 @@
     ],
     "data": [
         "security/ir.model.access.csv",
-        "security/project_security.xml",
         "views/project_project_view.xml",
         "views/project_participant_view.xml",
         "views/project_participant_rol_view.xml",
-        "views/res_config_settings_views.xml",
     ],
     "installable": True,
 }
