ce32c8f0e20a68c26b988e368749c09510c8a3c5
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-12-26 10:08:09 +0100

    [IMP] project_diary: add active field

--- project_diary/models/project_project.py
+++ project_diary/models/project_project.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Gontzal Gomez - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProjectProject(models.Model):
@@ -9,4 +9,13 @@ class ProjectProject(models.Model):
 
     diary_ids = fields.One2many(
         string='Project Diary', comodel_name='project.diary',
-        inverse_name='project_id')
+        inverse_name='project_id', copy=True, context={'active_test': False})
+
+    @api.multi
+    def write(self, vals):
+        res = super(ProjectProject, self).write(vals) if vals else True
+        if 'active' in vals:
+            # archiving/unarchiving a project does it on its diary, too
+            self.with_context(active_test=False).mapped('diary_ids').write(
+                {'active': vals['active']})
+        return res
