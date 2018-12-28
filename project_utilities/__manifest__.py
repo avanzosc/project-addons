b4decfd0046e20307c275833bdae75d077df6a64
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-12-28 11:35:13 +0100

    [IMP] project_utilities: New assistant in tasks, to add or subtract days.

--- project_utilities/__manifest__.py
+++ project_utilities/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Utilities",
-    "version": "11.0.1.1.0",
+    "version": "11.0.2.1.0",
     "license": "AGPL-3",
     "depends": [
         "project",
@@ -16,6 +16,7 @@
         "views/project_project_view.xml",
         "views/project_task_view.xml",
         "views/res_config_settings_view.xml",
+        "wizard/wiz_change_project_task_date_view.xml",
     ],
     "installable": True,
 }
--- project_utilities/models/__init__.py
+++ project_utilities/models/__init__.py
@@ -3,3 +3,4 @@
 
 from . import project_project
 from . import res_config_settings
+from . import project_task
