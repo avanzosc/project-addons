e7095c34d6643734181b7e53b8843053ef16d296
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-01-29 18:06:38 +0100

    [ADD] new module project_budget_expense

---
+++ project_budget_expense/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Bugdet Expense",
+    "version": "11.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "account_budget_expense",
+        "project_budget",
+        "project_task_cost",
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+}
---
+++ project_budget_expense/models/crossovered_budget.py
@@ -0,0 +1,8 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, models
+
+
+class CrossoveredBudget(models.Model):
+    _inherit = 'crossovered.budget'
