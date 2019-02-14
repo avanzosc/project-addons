f47487681d955595648263459322587bb9b29ca5
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-02-14 10:40:27 +0100

    [IMP] project_task_cost: create calendar with estimations

--- project_task_cost/models/project_task.py
+++ project_task_cost/models/project_task.py
@@ -2,9 +2,10 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from dateutil.relativedelta import relativedelta
+from datetime import timedelta
 from odoo import api, fields, models
 
-str2date = fields.Datetime.from_string
+str2date = fields.Date.from_string
 
 
 class ProjectTask(models.Model):
@@ -18,6 +19,8 @@ class ProjectTask(models.Model):
     planned_monthly_hours = fields.Float(
         string='Planned Monthly Hours',
         compute='_compute_planned_monthly_hours', store=True)
+    calendar_ids = fields.One2many(
+        comodel_name='project.task.calendar', inverse_name='task_id')
 
     @api.onchange('user_id')
     def _onchange_user(self):
@@ -38,9 +41,9 @@ class ProjectTask(models.Model):
     @api.depends('date_start', 'date_end', 'planned_hours')
     def _compute_planned_monthly_hours(self):
         for task in self.filtered(lambda t: t.date_start and t.date_end):
-            months = (
-                relativedelta(str2date(task.date_end),
-                              str2date(task.date_start)).months + 1)
+            datedelta = relativedelta(
+                str2date(task.date_end), str2date(task.date_start))
+            months = (datedelta.years * 12) + datedelta.months + 1
             task.planned_monthly_hours = task.planned_hours / (months or 1.0)
 
     @api.multi
@@ -50,3 +53,14 @@ class ProjectTask(models.Model):
             employee = employee_model.search(
                 [('user_id', '=', task.user_id.id)], limit=1)
             task.employee_cost = employee.timesheet_cost
+
+    @api.multi
+    def button_create_calendar(self):
+        for task in self.filtered(lambda t: t.date_start and t.date_end):
+            task.calendar_ids.unlink()
+            date_start = str2date(task.date_start)
+            date_end = str2date(task.date_end)
+            date_list = [date_start + timedelta(days=d)
+                         for d in range((date_end - date_start).days + 1)]
+            task.calendar_ids = [(0, 0, {'date': x,
+                                         'task_id': task.id}) for x in date_list]
