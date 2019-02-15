c51a386f224f11679ac54a46851de8b415262b66
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-02-15 12:49:51 +0100

    [IMP] project_task_cost: upgraded test

--- project_task_cost/models/project_project.py
+++ project_task_cost/models/project_project.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProjectProject(models.Model):
@@ -10,3 +10,7 @@ class ProjectProject(models.Model):
     resume_ids = fields.One2many(
         string='Resume', comodel_name='project.task.resume',
         inverse_name='project_id', readonly=True)
+
+    @api.multi
+    def button_create_task_calendar(self):
+        self.mapped('task_ids').button_create_calendar()
--- project_task_cost/tests/test_project_task_cost.py
+++ project_task_cost/tests/test_project_task_cost.py
@@ -3,7 +3,7 @@
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields
+from odoo import exceptions, fields
 from odoo.tests import common
 
 str2date = fields.Date.from_string
@@ -16,6 +16,9 @@ class TestProjectTaskCost(common.SavepointCase):
         super(TestProjectTaskCost, cls).setUpClass()
         cls.task_model = cls.env['project.task']
         employee_model = cls.env['hr.employee']
+        cls.project = cls.env['project.project'].create({
+            'name': 'Test Project',
+        })
         cls.employee = employee_model.search([
             ('user_id', '=', cls.env.user.id)], limit=1)
         if not cls.employee:
@@ -36,9 +39,11 @@ class TestProjectTaskCost(common.SavepointCase):
             'user_id': self.env.user.id,
             'date_start': date_start,
             'date_end': date_end,
+            'project_id': self.project.id,
         })
-        month_gap = relativedelta(
-            str2date(task.date_end), str2date(task.date_start)).months
+        datedelta = relativedelta(
+            str2date(task.date_end), str2date(task.date_start))
+        month_gap = (datedelta.years * 12) + datedelta.months
         self.assertFalse(task.employee_cost)
         task._onchange_user()
         self.assertEquals(task.employee_cost, self.timesheet_cost)
@@ -49,3 +54,18 @@ class TestProjectTaskCost(common.SavepointCase):
         task.invalidate_cache()
         self.assertEquals(
             task.planned_hours / (month_gap + 1), task.planned_monthly_hours)
+        self.assertFalse(task.calendar_ids)
+        self.project.button_create_task_calendar()
+        calendar_num = (
+            str2date(task.date_end) - str2date(task.date_start)).days + 1
+        self.assertEquals(len(task.calendar_ids), calendar_num)
+        with self.assertRaises(exceptions.ValidationError):
+            task.calendar_ids.create({
+                'task_id': task.id,
+                'date': date_start - relativedelta(days=10),
+            })
+        with self.assertRaises(exceptions.ValidationError):
+            task.calendar_ids.create({
+                'task_id': task.id,
+                'date': date_end + relativedelta(days=10),
+            })
