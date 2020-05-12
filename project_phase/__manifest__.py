0491e8e2aa2d146079bd93130818ed1fdf5b8d3d
Thor <alfredodelafuente@avanzosc.es>
2020-05-12 09:08:00 +0200

    [MIG] project_phase: Migration to 12.0

--- project_phase/__manifest__.py
+++ project_phase/__manifest__.py
@@ -3,15 +3,11 @@
 
 {
     "name": "Project Phase",
-    "version": "11.0.1.1.0",
+    "version": "12.0.1.0.0",
     "category": "Project",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>",
-    ],
     "depends": [
         "project",
     ],
