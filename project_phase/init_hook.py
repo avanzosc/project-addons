fd304f6f1093849ff7ca52c5f2731f67e07a8cb1
Thor <alfredodelafuente@avanzosc.es>
2020-05-12 09:32:16 +0200

    [MIG] project_phase: Migration to 12.0

--- project_phase/init_hook.py
+++ project_phase/init_hook.py
@@ -1,7 +1,6 @@
 # Copyright 2018 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-
 def post_init_hook(cr, registry):
     cr.execute("""
         UPDATE project_project
