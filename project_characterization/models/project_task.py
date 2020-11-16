5544efea471c38b6e336e7916316f6efdccbafb9
Imanol Aranburu <iaranburu@binovo.es>
2020-11-16 16:56:43 +0100

    [REF] 6221 Conflict project_characterization vs project_category

--- project_characterization/models/project_task.py
+++ project_characterization/models/project_task.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    type_id = fields.Many2one(
+    type2_id = fields.Many2one(
         comodel_name='project.task.type2', string='Type')
 
 
