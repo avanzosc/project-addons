a3163f14da10cd4e6d7742e1436a171fae73808b
oihane <oihanecruce@gmail.com>
2020-11-19 12:00:01 +0100

    [ADD] New module project_sale_utilities (#222)

---
+++ project_sale_utilities/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Sale Utilities",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "sale",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Project",
+    "data": [
+        "views/account_invoice_line_view.xml",
+        "views/project_project_view.xml",
+    ],
+    "installable": True,
+}
