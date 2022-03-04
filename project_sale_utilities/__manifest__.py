03b266d937733a71d05ff05643d277a5badeea0f
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-03-04 12:54:45 +0100

    [MIG] project_sale_utilities: Migration to 12.0

--- project_sale_utilities/__manifest__.py
+++ project_sale_utilities/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Sale Utilities",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "project",
@@ -13,7 +13,6 @@
     "website": "http://www.avanzosc.es",
     "category": "Project",
     "data": [
-        "views/account_invoice_line_view.xml",
         "views/project_project_view.xml",
     ],
     "installable": True,
