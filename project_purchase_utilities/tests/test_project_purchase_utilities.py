a74597d171502fcaa0fab9615be014a4b3d805f4
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-03-04 10:39:08 +0100

    [FIX] project_purchase_utilities: show only vendor bill lines

--- project_purchase_utilities/tests/test_project_purchase_utilities.py
+++ project_purchase_utilities/tests/test_project_purchase_utilities.py
@@ -6,6 +6,8 @@ from odoo.tests import common
 from odoo.osv import expression
 
 
+@common.at_install(False)
+@common.post_install(True)
 class TestProjectPurchaseUtilities(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
@@ -27,16 +29,16 @@ class TestProjectPurchaseUtilities(common.SavepointCase):
         })
         self.assertFalse(self.project.purchase_count)
         self.assertFalse(self.project.purchase_line_count)
-        self.assertFalse(self.project.purchase_invoice_count)
-        self.assertFalse(self.project.purchase_invoice_line_count)
+        self.assertFalse(self.project.in_invoice_count)
+        self.assertFalse(self.project.in_invoiced)
         self.purchase.order_line[:1].write({
             'account_analytic_id': self.project.analytic_account_id.id,
         })
         self.project.invalidate_cache()
         self.assertEquals(self.project.purchase_count, 1)
         self.assertEquals(self.project.purchase_line_count, 1)
-        self.assertFalse(self.project.purchase_invoice_count)
-        self.assertFalse(self.project.purchase_invoice_line_count)
+        self.assertFalse(self.project.in_invoice_count)
+        self.assertFalse(self.project.in_invoiced)
         self.purchase.button_confirm()
         invoice = self.invoice_model.create({
             'partner_id': self.purchase.partner_id.id,
@@ -45,8 +47,9 @@ class TestProjectPurchaseUtilities(common.SavepointCase):
         })
         invoice.purchase_order_change()
         self.project.invalidate_cache()
-        self.assertEquals(self.project.purchase_invoice_count, 1)
-        self.assertEquals(self.project.purchase_invoice_line_count, 1)
+        self.assertEquals(self.project.in_invoice_count, 1)
+        self.assertEquals(self.project.in_invoiced,
+                          sum(invoice.mapped("invoice_line_ids.price_subtotal_signed")))
         purchase_domain = [
             ('account_analytic_id', 'in',
              self.project.mapped('analytic_account_id').ids)]
@@ -58,13 +61,13 @@ class TestProjectPurchaseUtilities(common.SavepointCase):
         purchase_line_dict = self.project.button_open_purchase_order_line()
         self.assertEquals(
             purchase_line_dict.get('domain'), purchase_domain)
-        invoice_lines = self.env['account.invoice.line'].search(
-            purchase_domain)
-        invoice_action = self.browse_ref('purchase.action_invoice_pending')
+        invoice_lines = self.project._get_in_invoices()
+        invoice_action = self.browse_ref('account.action_invoice_tree')
         invoice_domain = expression.AND([
             [('id', 'in', invoice_lines.mapped('invoice_id').ids)],
             safe_eval(invoice_action.domain or '[]')])
         invoice_dict = self.project.button_open_purchase_invoice()
         self.assertEquals(invoice_dict.get('domain'), invoice_domain)
+        invoice_line_domain = self.project._get_in_invoices(domain=True)
         invoice_line_dict = self.project.button_open_purchase_invoice_line()
-        self.assertEquals(invoice_line_dict.get('domain'), purchase_domain)
+        self.assertEquals(invoice_line_dict.get('domain'), invoice_line_domain)
