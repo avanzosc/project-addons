19d77ac32316a68623b839c8c117f43bb400bfb7
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-30 16:33:09 +0200

    [IMP] project_sale_usability: black, isort, prettier

--- project_vehicle/models/account_analytic_line.py
+++ project_vehicle/models/account_analytic_line.py
@@ -1,16 +1,15 @@
 # Copyright 2021 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccounAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     vehicle_id = fields.Many2one(
-        string='Vehicle', comodel_name='fleet.vehicle',
-        compute='_compute_vehicle_id')
+        string="Vehicle", comodel_name="fleet.vehicle", compute="_compute_vehicle_id"
+    )
 
     def _compute_vehicle_id(self):
         for analytic in self:
-            analytic.vehicle_id = (
-                analytic.account_id.project_ids[:1].vehicle_id)
+            analytic.vehicle_id = analytic.account_id.project_ids[:1].vehicle_id
--- project_vehicle/models/project_task.py
+++ project_vehicle/models/project_task.py
@@ -1,13 +1,14 @@
 # Copyright 2021 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     vehicle_id = fields.Many2one(
-        string='Vehicle',
-        comodel_name='fleet.vehicle',
-        related='project_id.vehicle_id',
-        store=True)
+        string="Vehicle",
+        comodel_name="fleet.vehicle",
+        related="project_id.vehicle_id",
+        store=True,
+    )
