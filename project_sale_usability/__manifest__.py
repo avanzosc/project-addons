46026b130502f42d3c3b845873defa322a90ee72
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-30 16:39:17 +0200

    [MIG] project_sale_usability: Migration to 14.0

---
+++ project_sale_usability/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Sale Usability",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "sale",
+        "sales_team",
+    ],
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/project-addons",
+    "category": "Project",
+    "data": [
+        "views/project_project_view.xml",
+    ],
+    "installable": True,
+}
---
+++ project_sale_usability/tests/test_project_sale_utilities.py
@@ -0,0 +1,98 @@
+# Copyright 2020 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from collections import OrderedDict
+
+from odoo.osv import expression
+from odoo.tests import common, tagged
+from odoo.tools.safe_eval import safe_eval
+
+
+@tagged("post_install", "-at_install")
+class TestProjectSaleUtilities(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestProjectSaleUtilities, cls).setUpClass()
+        cls.products = OrderedDict(
+            [
+                ("prod_order", cls.env.ref("product.product_order_01")),
+                ("prod_del", cls.env.ref("product.product_delivery_01")),
+            ]
+        )
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "Test Customer",
+            }
+        )
+        cls.project = cls.env["project.project"].create(
+            {
+                "name": "Test Project",
+                "partner_id": cls.partner.id,
+            }
+        )
+        cls.project._create_analytic_account()
+        cls.sale_model = cls.env["sale.order"]
+        cls.sale = cls.sale_model.create(
+            {
+                "partner_id": cls.partner.id,
+                "analytic_account_id": cls.project.analytic_account_id.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": p.name,
+                            "product_id": p.id,
+                            "product_uom_qty": 2,
+                            "product_uom": p.uom_id.id,
+                            "price_unit": p.list_price,
+                        },
+                    )
+                    for p in cls.products.values()
+                ],
+            }
+        )
+        cls.invoice_model = cls.env["account.move"]
+
+    def test_project_sale_out_invoice(self):
+        inv = self.create_invoice_from_sale_order()
+        self.assertEqual(self.project.out_invoice_count, 1.0)
+        self.assertEqual(
+            self.project.out_invoiced,
+            sum(inv.mapped("invoice_line_ids.price_subtotal")),
+        )
+        line_domain = [
+            (
+                "analytic_account_id",
+                "in",
+                self.project.mapped("analytic_account_id").ids,
+            ),
+            ("move_id.move_type", "=", "out_invoice"),
+        ]
+        invoice_lines = self.env["account.move.line"].search(line_domain)
+        invoice_action = self.browse_ref("account.action_move_out_invoice_type")
+        invoice_domain = expression.AND(
+            [
+                [("id", "in", invoice_lines.mapped("move_id").ids)],
+                safe_eval(invoice_action.domain or "[]"),
+            ]
+        )
+        invoice_dict = self.project.button_open_out_invoice()
+        self.assertEqual(invoice_dict.get("domain"), invoice_domain)
+        invoice_line_domain = self.project._get_out_invoiced(domain=True)
+        invoice_line_dict = self.project.button_open_out_invoice_line()
+        self.assertEqual(invoice_line_dict.get("domain"), invoice_line_domain)
+
+    def create_invoice_from_sale_order(self):
+        self.assertTrue(self.sale.state == "draft")
+        self.sale.action_confirm()
+        self.assertTrue(self.sale.state == "sale")
+        self.assertTrue(self.sale.invoice_status == "to invoice")
+        inv = self.sale._create_invoices()
+        self.assertTrue(
+            self.sale.invoice_status == "no",
+            'Sale: SO status after invoicing should be "nothing to invoice"',
+        )
+        self.assertEqual(len(inv.invoice_line_ids), 1, "Sale: invoice is missing lines")
+        self.assertTrue(len(self.sale.invoice_ids) == 1, "Sale: invoice is missing")
+        return inv
