303e841adaf34f0f8c46da11e424a9c9f2ca85d1
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-31 09:55:40 +0200

    [MIG] project_purchase_usability: Migration to 14.0

---
+++ project_purchase_usability/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Gontzal Gomez - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Purchase Usability",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "purchase",
+    ],
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/project-addons",
+    "category": "Project",
+    "data": [
+        "views/project_project_view.xml",
+    ],
+    "installable": True,
+}
