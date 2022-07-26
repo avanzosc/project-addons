4f1f10ebbef1736edbabd505f7a5cb7a53c36b77
Thor <alfredodelafuente@avanzosc.es>
2022-07-26 09:48:48 +0200

    [ADD] project_timeline_employee: In project task, in "timeline" form view, show "Employee".

---
+++ project_timeline_employee/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Project Timeline Employee",
+    "version": "12.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/project-addons",
+    "depends": [
+        "project_timeline",
+        "project_task_employee"
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
