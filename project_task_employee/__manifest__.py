5a8f4c0d433a3b67e41e31c756afdf7fc040d1f1
Thor <alfredodelafuente@avanzosc.es>
2022-07-26 09:20:26 +0200

    [ADD] project_task_employee: New field "Employee" in project tasks.

---
+++ project_task_employee/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Project Task Employee",
+    "version": "12.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/project-addons",
+    "depends": [
+        "project",
+        "hr"
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+    "installable": True,
+}
