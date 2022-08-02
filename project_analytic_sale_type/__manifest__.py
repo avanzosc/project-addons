d5f8fa2e797e88216036ea7e5c0dd93cedc5572e
Thor <alfredodelafuente@avanzosc.es>
2022-08-02 11:31:21 +0200

    [ADD] project_analytic_sale_type: Sale type in projects, analytic accounts, and analytic items.

---
+++ project_analytic_sale_type/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022 Alfredo de la Fuente - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Analytic Sale type",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "analytic",
+        "sale",
+        "project",
+        "sale_order_type",
+    ],
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "summary": """Project Analytic Sale Type""",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "data": [
+        "views/account_analytic_account_views.xml",
+        "views/account_analytic_line_views.xml",
+        "views/project_project_views.xml",
+        ],
+    "installable": True,
+    "auto_install": False,
+}
