eff2a7ea8cea26dbac281837ebe129f14aa3580b
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-10-20 13:01:53 +0200

    [ADD] project_analytic_sale_type: Sale type in projects, analytic accounts, and analytic items. (#263)

---
+++ project_analytic_sale_type/models/project_project.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022 Alfredo de la Fuente - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    sale_type_id = fields.Many2one(
+        string="Sale type", comodel_name="sale.order.type",
+        related="sale_order_id.type_id", store=True, copy=False)
