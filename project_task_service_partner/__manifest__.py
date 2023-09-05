45223dce3d517c484f0d27ea0bcb6e2f6f276727
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-05 14:44:51 +0200

    [MIG] project_task_service_partner: Migration to 16.0

--- project_task_service_partner/__manifest__.py
+++ project_task_service_partner/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project task service partner",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/project-addons",
-    "category": "Sales/CRM",
+    "category": "Hidden",
     "depends": [
         "project",
         "contacts",
