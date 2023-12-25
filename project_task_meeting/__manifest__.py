7d4680e3504cc1c56e64816c8db4b8c832836a5d
Unai Beristain <unaiberis@gmail.com>
2023-12-25 16:57:10 +0100

    [MIG] project_task_meeting: Migration to 16.0

--- project_task_meeting/__manifest__.py
+++ project_task_meeting/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Project Task Meeting",
-    "version": "14.0.2.1.0",
+    "version": "16.0.1.0.0",
     "category": "Project",
     "license": "AGPL-3",
     "author": "AvanzOSC",
