b6f8321db8e96e99d4facf8f4140eb998c35f645
Unai Beristain <unaiberis@gmail.com>
2024-06-25 16:28:08 +0200

    [ADD] Add module project_Mrp

---
+++ project_mrp/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Project MRP Integration",
+    "version": "1.0",
+    "category": "Project",
+    "website": "http://www.avanzosc.es",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "depends": ["base", "project", "mrp", "sale", "mrp_sale_info"],
+    "data": [
+        "views/project_task_views.xml",
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
