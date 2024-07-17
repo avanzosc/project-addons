d71da193099876218fc31c179227742d60edc7c4
Unai Beristain <unaiberis@gmail.com>
2024-07-17 13:56:23 +0200

    [IMP] project_task_meeting

--- project_task_meeting/models/calendar_event.py
+++ project_task_meeting/models/calendar_event.py
@@ -2,11 +2,30 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import api, fields, models
 
+from odoo.addons.calendar.models.calendar_attendee import Attendee
+
 
 class CalendarEvent(models.Model):
     _inherit = "calendar.event"
 
-    task_id = fields.Many2one(string="Task", comodel_name="project.task")
+    attendee_status = fields.Selection(
+        Attendee.STATE_SELECTION,
+        string="Attendee Status",
+        compute="_compute_attendee",
+        store="True",
+    )
+
+    task_id = fields.Many2one(
+        string="Task",
+        comodel_name="project.task",
+    )
+
+    task_partner_id = fields.Many2one(
+        "res.partner",
+        string="Task Customer",
+        related="task_id.partner_id",
+        store=True,
+    )
 
     @api.model
     def _get_public_fields(self):
