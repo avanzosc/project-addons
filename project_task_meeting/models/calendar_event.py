f770f706f30f724b1d2ee3394626b3ece83aee69
Unai Beristain <unaiberis@gmail.com>
2024-07-17 13:56:23 +0200

    [IMP] project_task_meeting

--- project_task_meeting/models/calendar_event.py
+++ project_task_meeting/models/calendar_event.py
@@ -6,10 +6,36 @@ from odoo import api, fields, models
 class CalendarEvent(models.Model):
     _inherit = "calendar.event"
 
-    task_id = fields.Many2one(string="Task", comodel_name="project.task")
+    task_id = fields.Many2one(
+        string="Task",
+        comodel_name="project.task",
+    )
+
+    task_partner_id = fields.Many2one(
+        "res.partner",
+        string="Task Customer",
+        related="task_id.partner_id",
+        store=True,
+    )
 
     @api.model
     def _get_public_fields(self):
         result = super(CalendarEvent, self)._get_public_fields()
         result |= {"task_id"}
         return result
+
+    @api.model
+    def read_group(
+        self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True
+    ):
+        groupby = [groupby] if isinstance(groupby, str) else groupby
+        groupby = [field for field in groupby if field.split(":")[0] != "state"]
+        return super(CalendarEvent, self).read_group(
+            domain,
+            fields,
+            groupby,
+            offset=offset,
+            limit=limit,
+            orderby=orderby,
+            lazy=lazy,
+        )
