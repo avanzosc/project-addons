99eab9f299daa3641cc919699aea125940fd9030
Allan Aguilar <aaguilar@binovo.es>
2026-03-25 13:16:42 +0100

    [16.0][IMP] project_hours_progress: show milestone fields depending project configuration

--- project_hours_progress/__manifest__.py
+++ project_hours_progress/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Project Hours Progress",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Services/Project",
     "license": "AGPL-3",
     "author": "AvanzOSC",
