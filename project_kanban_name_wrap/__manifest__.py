0c3152119a8146f35de6684887c0a2b536ff7437
aneravanzosc <aneravanzosc@gmail.com>
2026-05-20 14:22:16 +0200

    [16.0][ADD] project_kanban_name_wrap: wrap long names in project

---
+++ project_kanban_name_wrap/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Aner Arregi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Project Kanban Name Wrap",
+    "version": "16.0.1.0.0",
+    "category": "Project",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/project-addons",
+    "license": "LGPL-3",
+    "depends": ["project"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "project_kanban_name_wrap/static/src/scss/project_kanban.scss",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
