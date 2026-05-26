8d2fec0770e37a1c1e338cb72ca3e6dea1aa27a0
Alfredo <alfredodelafuente@avanzosc.es>
2026-05-26 09:09:07 +0200

    [18.0][ADD] project_purchase_usability: In purchase order "tree" and "search" views, show project.

---
+++ project_purchase_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Project Purchase Usability",
+    "version": "18.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/project-addons",
+    "depends": [
+        "project_purchase",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
